ff0af20dec0291b54cb48d65bbbf32b1b800911e
Da hee <ekdudekgml35@naver.com>
2021-12-29 10:27:54 +0900

    New 스크린으로 이동하게 변경함

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_movie/ui/home/home_screen.dart';
+import 'package:flutter_movie/ui/new/new_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const NewScreen(),
     );
   }
 }
